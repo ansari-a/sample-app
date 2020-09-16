299a5b4f9e735ca1ec8c95348d9d83f60c816f70
Abolfazl Ansari <abolfazl.ansari@gmail.com>
2020-09-16 14:02:28 +0430

    base project files added

---
+++ src/main/kotlin/ir/pegahtech/mediaad/sample/SampleAppApplication.kt
@@ -0,0 +1,11 @@
+package ir.pegahtech.mediaad.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SampleAppApplication>(*args)
+}
---
+++ src/test/kotlin/ir/pegahtech/mediaad/sample/SampleAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package ir.pegahtech.mediaad.sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
